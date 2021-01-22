ac8387283ad2f9197dad0ce68cc9baec3c6f2a2b
Christian Stangier <christian@stangier.email>
2021-01-22 18:06:58 +0100

    Setup test frameworks for verifying message exchange

--- Sources/HAClient/HAClient.swift
+++ Sources/HAClient/HAClient.swift
@@ -1,68 +1,94 @@
 import Foundation
+
+protocol MessageExchange {
+    mutating func setMessageHandler(_ messageHandler: @escaping ((String) -> Void))
+    func sendMessage(message: String)
+    func ping()
+}
+
+struct HAClient {
+    private var messageExchange: MessageExchange
+
+    init(messageExchange: MessageExchange) {
+        self.messageExchange = messageExchange
+        self.messageExchange.setMessageHandler(onMessage)
+    }
+
+    func authenticate(token: String) {
+        messageExchange.sendMessage(message: "token foo")
+    }
+
+    private func onMessage(message: String) {
+        print("Client received message from websocket \(message)")
+    }
+}
+
 class WebSocket: NSObject, URLSessionWebSocketDelegate {
     func urlSession(_ session: URLSession, webSocketTask: URLSessionWebSocketTask, didOpenWithProtocol protocol: String?) {
         print("Web Socket did connect")
     }
+
     func urlSession(_ session: URLSession, webSocketTask: URLSessionWebSocketTask, didCloseWith closeCode: URLSessionWebSocketTask.CloseCode, reason: Data?) {
         print("Web Socket did disconnect")
     }
 }
 
-struct WebSocketConnection {
+struct WebSocketConnection: MessageExchange {
     private let websocketEndpoint: String
     private let webSocketDelegate = WebSocket()
     private let webSocketTask: URLSessionWebSocketTask
     private var messageHandler: ((String) -> Void)?
-    
+
     init(endpoint: String) {
-        self.websocketEndpoint = endpoint
-        
+        websocketEndpoint = endpoint
+
         let session = URLSession(
             configuration: .default,
             delegate: webSocketDelegate,
             delegateQueue: OperationQueue()
         )
+
         let url = URL(string: websocketEndpoint)!
-        self.webSocketTask = session.webSocketTask(with: url)
+        webSocketTask = session.webSocketTask(with: url)
         webSocketTask.resume()
+        receiveMessage()
     }
-    
+
     mutating func setMessageHandler(_ messageHandler: @escaping ((String) -> Void)) {
         self.messageHandler = messageHandler
     }
-    
+
     func sendMessage(message: String) {
-        webSocketTask.send(URLSessionWebSocketTask.Message.string(message))
-        { error in
+        webSocketTask.send(URLSessionWebSocketTask.Message.string(message)) { error in
             if let error = error {
                 print("WebSocket sending error: \(error)")
             }
         }
     }
-    
-    func receiveMessage() {
+
+    private func receiveMessage() {
         webSocketTask.receive { result in
-                switch result {
-                case .failure(let error):
-                    print("Failed to receive message: \(error)")
-                case .success(let message):
-                    switch message {
-                    case .string(let text):
-                        print("Received text message: \(text)")
-                        if let messageHandler = self.messageHandler {
-                            messageHandler(text)
-                        }
-                    case .data(let data):
-                        print("Received binary message: \(data)")
-                    @unknown default:
-                        fatalError()
+            switch result {
+            case let .failure(error):
+                print("Failed to receive message: \(error)")
+            case let .success(message):
+                switch message {
+                case let .string(text):
+                    print("Received text message: \(text)")
+                    if let messageHandler = self.messageHandler {
+                        messageHandler(text)
                     }
-                    
-                    self.receiveMessage()
+                case let .data(data):
+                    print("Received binary message: \(data)")
+                @unknown default:
+                    fatalError()
                 }
+
+                self.receiveMessage()
             }
+        }
     }
-    
+
     func ping() {
         print("sending a ping")
         webSocketTask.sendPing { error in
@@ -72,18 +98,5 @@ struct WebSocketConnection {
                 print("Successfully sent a ping")
             }
         }
-    }
-}
-
-struct HAClient {
-    private var websocketConnection: WebSocketConnection
-    
-    init(websocketConnection: WebSocketConnection) {
-        self.websocketConnection = websocketConnection
-        self.websocketConnection.setMessageHandler(self.onMessage)
-    }
-    
-    func onMessage(message: String) {
-        print("Client received message from websocket \(message)")
     }
 }
--- Tests/HAClientTests/HAClientTests.swift
+++ Tests/HAClientTests/HAClientTests.swift
@@ -1,10 +1,34 @@
+import Nimble
 import XCTest
+
 @testable import HAClient
 
+class FakeMessageExchange: MessageExchange {
+    var sentMessages: [String] = []
+
+    func setMessageHandler(_ messageHandler: @escaping ((String) -> Void)) {
+    }
+
+    func sendMessage(message: String) {
+        sentMessages.append(message)
+    }
+
+    func ping() {
+    }
+}
+
 final class HAClientTests: XCTestCase {
-    func testExample() {
-//        let client = HAClient(endpoint: "ws://192.168.86.42:8123/api/websocket")
-//
-//        client.ping()
+    var mockExchange: FakeMessageExchange?
+    var client: HAClient?
+
+    override func setUp() {
+        mockExchange = FakeMessageExchange()
+        client = HAClient(messageExchange: mockExchange!)
+    }
+
+    func testSendsAccessTokenAfterConnection() {
+        client?.authenticate(token: "token")
+        // TODO: expect JSON string
+        expect(self.mockExchange?.sentMessages).to(equal(["token foo"]))
     }
 }
